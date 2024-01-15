1f09ab48ef53da211208ee1a4023bd95582960a9
Joe D <joedef@gmail.com>
2024-01-15 15:00:06 -0500

    my-restore revamp: incremental update

--- InlineMenu.py
+++ InlineMenu.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python3
-""" A simple menu tool w/o the full curses ado."""
+""" A simple menu tool w/o the full curses ado.
+Create the menu by passing in:
+- an optional 'default' key where the menu starts
+    - if default is an object with attribute 'next', that will be use
+- an optional 'title'
+- 'prompts' which is a dict of the form {key: prompt, ...} where:
+    - 'key' is one character (typically abc...012..ABC...)
+        - often 'x' is reserved for exiting the menu if looped
+    - each 'prompt' may be:
+        - a single line string
+        - a multiple line string
+            - the first line is the actual prompt
+            - the remainder is called extra
+        - list or tuple of values:
+            - the first element is a single or multiple line string (as above)
+            - if no more elements, 'payload' is None
+            - if one more element, 'payload' is prompt[1]
+            - if more than one element, 'payload' is prompt[1:]
+
+- Menu.prompt() just returns the key
+- Menu.get_prompt_obj() return a SimpleNamespace with members:
+    - key: the key selected
+    - prompt: the prompt shown
+    - extra: described above (None of only single line given)
+    - payload: described above when prompt is a list or tuple
+    - next: the next key
+"""
 # pylint: disable=invalid-name,broad-except,too-many-instance-attributes
 # pylint: disable=too-few-public-methods
 
@@ -8,6 +34,7 @@ import termios
 import tty
 import shutil
 import signal
+from types import SimpleNamespace
 
 ####################################################################################
 
@@ -44,7 +71,7 @@ class Menu:
 
     class InputTimeout(Exception):
         """ Raised for obvious reasons. """
-
+        
     @staticmethod
     def _prompt_mode(enable=False):
         """ Used to start / complete prompting the user so that we can read
@@ -61,22 +88,47 @@ class Menu:
 
     def __init__(self, prompts, default=None, title=''):
         self.prompts = prompts
-        self.lines = []
+        self.lines, self.keys = [], []
+        self.objects = {}
         self.selected = len(prompts)
         self.title = title
         self.cols, self.rows = shutil.get_terminal_size()
         self.max_line = 0
         # time.sleep(10)
-        self.pos = len(prompts)
-        for idx, (key, prompt) in enumerate(self.prompts.items()):
-            self.lines.append(f'{key}: {prompt}')
+        self.pos = 0
+        self._parse_prompts()
         self.input = ''
         self.default_bottom = 'Use Up/Down/key to highlight and Enter to select'
         self.prev_bottom = self.default_bottom
         self._refresh(clear=False)
+        self.pos = len(self.lines)
+        if hasattr(default, 'next'):
+            default = default.next
+        default = default if default else self.keys[0]
         if default in self.prompts:
-            idx = list(self.prompts.keys()).index(default)
+            idx = list(self.keys).index(default)
             self._move(idx - self.selected)
+
+    def _parse_prompts(self):
+        for key, prompt in self.prompts.items():
+            payload, extra = None, None
+            if key in self.objects:
+                print(f'WARNING: skip dup key ({key})')
+                continue
+            if isinstance(prompt, (tuple, list)):
+                prompt, payload = prompt[0], prompt[1:]
+                payload = (None if not payload else
+                           payload[0] if len(payload) == 1 else payload)
+            lines = prompt.splitlines()
+            if len(lines) > 1:
+                prompt, extra = lines[0], '\n'.join(lines[1:])
+            prompt = prompt.rstrip()
+            self.lines.append(f'{key}: {prompt}')
+            self.keys.append(key)
+            self.objects[key] = SimpleNamespace(key=key, prompt=prompt,
+                                   extra=extra, payload=payload, next=None)
+        for idx, ns in enumerate(self.objects.values()):
+            ns.next = self.keys[ min(idx+1, len(self.keys)-1) ]
 
     def _refresh(self, clear=True):
         """ Draw/redraw the menu optionally clearing the screen.
@@ -198,8 +250,6 @@ class Menu:
         for idx in range(len(self.lines)):
             print(self._set_pos_str(idx) + Term.erase_line(), end='')
         print(Term.pos_down(1) + Term.erase_line(), end='', flush=True)
-        
-        
         action = Term.pos_up(1+len(self.lines)) + Term.erase_line()
         action += Term.reverse_video() + 'PICK' + Term.normal_video() + ': '
         action += self.lines[self.selected][:self.cols-6]
@@ -210,9 +260,36 @@ class Menu:
         if self.prev_bottom != self.default_bottom:
             self._bottom_line(self.default_bottom)
 
+    @staticmethod
+    def get_next_key(key):
+        """ Returns the next ASCII char after the given;
+            e.g., 'b' = get_next_key('a')
+        """
+        # old_int = ord(key[0])
+        # next_int = old_int + 1
+        # next_chr = chr(next_int)
+        # next_str = str(next_chr)
+        # return next_str
+        return str(chr(1+ord(key[0])))
+    def get_command(self, obj):
+        """ Return the 'command' from the returned Namespace which
+            by convention is the 'extra', the 'paload' if a string,
+            or the prompt ... in that priority order
+        """
+        if obj.extra:
+            return obj.extra
+        if isinstance(obj.payload, str):
+            return obj.payload
+        return obj.prompt
+
     def prompt(self):
+        """ The simplified external entry point which prompts the user
+        to select and choose a menu entry and returns the key  """
+        return self.get_prompt_obj().key
+
+    def get_prompt_obj(self):
         """ The external entry point which prompts the user
-        to select and choose a menu entry  """
+        to select and choose a menu entry an returns a namespace  """
         while True:
             try:
                 self._prompt_mode(enable=True)
@@ -235,7 +312,7 @@ class Menu:
                         self._prompt_mode(enable=False)
                         # self.finish(f'\n\nrunning {repr(picked)}')
                         self._finish()
-                        return picked
+                        return self.objects[picked]
                     self._bottom_line('invalid(no selection); pick again')
                 elif ans in self.prompts:
                     self._restore_default_bottom()
@@ -248,16 +325,69 @@ class Menu:
 
 def runner(_): # def runner(argv):
     """ TBD """
-    while True:
-        opts = {
-            'q': 'Quit',
-            'A': 'Set root password [suggested: "pw"]',
-            'B': 'Set current user password [suggested: "pw"]',
-            'u': 'Update Linux -- run after ChromeOS update (at least)',
-            'r': 'Refresh Icons -- fix cases of icons becoming lost',
-        }
-        menu = Menu(opts, 'u')
-        picked = opts[menu.prompt()]
-        print(f'\n===> To run: {picked}\n')
-        if 'Quit' in picked:
-            sys.exit(0)
+    
+    def simple_menu():
+        default = 'A'
+        while True:
+            opts = {
+                'q': 'Quit',
+                'A': 'Set root password [suggested: "pw"]',
+                'B': 'Set current user password [suggested: "pw"]',
+                'u': 'Update Linux -- run after ChromeOS update (at least)',
+                'r': 'Refresh Icons -- fix cases of icons becoming lost',
+            }
+            menu = Menu(opts, default=default, title='Simple Menu')
+            key = menu.prompt()
+            line = opts[key]
+            print(f'\n===> To run: {line}\n')
+            if 'Quit' in line:
+                return 0
+            default = menu.objects[key].next
+
+    def multiline_menu():
+        picked = 'A'
+        while True:
+            opts = {
+                'A': 'Set root password [suggested: "pw"]\n'
+                     'sudo passwd',
+                'B': 'Set current user password [suggested: "pw"]\n'
+                     'passwd',
+                'u': 'Update Linux -- run after ChromeOS update (at least)\n'
+                     'sudo pacman -Syu',
+                'r': 'Refresh Icons -- fix cases of icons becoming lost\n'
+                     'fix passwords somehow',
+                'x': 'exit\n'
+                     '# return from menu loop',
+            }
+            menu = Menu(opts, picked, title='MultiLine Menu')
+            picked = menu.get_prompt_obj()
+            print(f'\n===> To run: {picked.extra}\n')
+            if 'exit' in picked.prompt:
+                return 0
+
+    def mixed_menu():
+        picked = None
+        while True:
+            opts = {
+                'A': 'Set root password [suggested: "pw"]\n'
+                     'sudo passwd',
+                'B': 'Set current user password [suggested: "pw"]\n'
+                     'passwd',
+                'u': ('Update Linux -- run after ChromeOS update (at least)\n'
+                     'sudo pacman -Syu', {'key1': 'val1', 'key2': 'val2'}),
+                'r': ('Refresh Icons -- fix cases of icons becoming lost',
+                     {'Key1': 'Val1', 'Key2': 'Val2'}),
+                'q': 'Quit\n',
+            }
+            menu = Menu(opts, picked, title='Mixed Menu')
+            picked = menu.get_prompt_obj()
+            print(f'\n===> returned: {vars(picked)}')
+            if 'Quit' in picked.prompt:
+                return 0
+    
+    simple_menu()
+    multiline_menu()
+    mixed_menu()
+
+if __name__ == '__main__':
+    runner(sys.argv)
